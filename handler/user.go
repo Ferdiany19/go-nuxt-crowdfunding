eb8a3a76c09a4e6f402585406b587ad8947a4ba4
ferdiany19 <ferdianyuliansyah19@gmai.com>
2021-07-12 22:15:22 +0700

    Login API

--- handler/user.go
+++ handler/user.go
@@ -44,3 +44,32 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+
+		// menampilkan pesan error menggunakan array errors
+		errMsg := gin.H{"errors": errors}
+		response := helper.ApiResponse("Login Failed!", http.StatusUnprocessableEntity, "error", errMsg)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+	if err != nil {
+		errMsg := gin.H{"errors": err.Error()}
+		response := helper.ApiResponse("Login Failed!", http.StatusUnprocessableEntity, "error", errMsg)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	// mengembalikan ke json
+	formatter := user.FormatUser(loggedinUser, "token")
+	response := helper.ApiResponse("Login Successfully!", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
